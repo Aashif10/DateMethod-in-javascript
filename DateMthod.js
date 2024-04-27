b58a333dd3062c726a2fd0389142bda2cda26a7c
Aashif <aashif3225@gmail.com>
2024-04-28 01:21:20 +0530

    Date Methods

---
+++ DateMthod.js
@@ -0,0 +1,64 @@
+// Get current date using Date Method in javascript
+
+//getFullYear()
+/*
+const date=new Date()// Date method constructor
+let d=date.getFullYear();
+console.log(d); */
+
+
+// getMonth()
+ /*
+const date=new Date()// Date method constructor
+let d=date.getMonth();
+console.log(d);// return 0-11 number according to current Month
+ // To find in string type Month 
+ */
+/*
+ var monthNames = [ "January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December" ];
+ let mon=new Date();
+ let month=monthNames[mon.getMonth()]
+console.log(month);*/
+
+/*
+//getDay()
+let date=new Date();//Date method constructor
+let d=date.getDay()
+console.log(d);*/
+  
+/*
+//To find string type days
+let day=['Sunday','Monday',"Tuesday","Wednesday",'Thursday','Friday','Saturday'];
+let date=new Date()//Date method constructor
+let days=day[date.getDay()]
+console.log(days); */
+
+//getTime()
+/*
+let date=new Date()
+let time=date.getTime();
+console.log(time);//return time in millesecond
+*/
+
+// getHours()
+/*
+let date=new Date()//Date method constructor
+let hour=date.getHours()
+console.log(hour);
+*/
+// getminutes()
+/*
+let date=new Date()
+let min=date.getMinutes();
+console.log(min); */
+
+// getseonds()
+/*
+let date=new Date();
+let sec=date.getSeconds()
+console.log(sec);*/
+
+
+// This is all about Date method Here
+
